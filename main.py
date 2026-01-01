67dfa5e16d5e0713a015bb7ab8aab1b020399c86
julianjackson <20231881+isjulianj@users.noreply.github.com>
2026-01-01 12:35:20 +0000

    Added asteroids, and collision

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import sys
+
 import pygame
-from logger import log_state
+from logger import log_state, log_event
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_RADIUS
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -10,11 +14,13 @@ def main():
     ## groups
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
-
+    asteroids = pygame.sprite.Group()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    Player.containers = (updatable, drawable)
+    Player.containers = (updatable, drawable) 
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2), PLAYER_RADIUS)
-    
+    asteroid_field = AsteroidField()
     print(f"Starting Asteroids with pygame version {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -33,6 +39,11 @@ def main():
             print(event)
         screen.fill("black")
         updatable.update(dt)
+        for a in asteroids:
+            if a.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
         for p in drawable:
             p.draw(screen)
         pygame.display.flip()
---
+++ shot.py
@@ -0,0 +1,8 @@
+import pygame
+from circleshape import CircleShape
+
+
+
+class Shot(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, )
